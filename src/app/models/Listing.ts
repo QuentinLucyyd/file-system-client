697befde663d91123d3da8401e38b67b194b77b8
QuentinLucyyd <quintonacid@gmail.com>
2021-11-26 11:51:10 +0200

    Added path to display

--- src/app/models/Listing.ts
+++ src/app/models/Listing.ts
@@ -1,13 +1,15 @@
 export class Listing {
   name: string;
+  path: string;
   size: string;
   directory: boolean;
   created: string;
   extension: string;
   permissions: string;
 
-  constructor(name: string, size: string, directory: boolean, created: string, extension: string, permissions: string) {
+  constructor(name: string, path:string,  size: string, directory: boolean, created: string, extension: string, permissions: string) {
     this.name = name;
+    this.path = path;
     this.size = size;
     this.directory = directory;
     this.created = created;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http:/localhost:4040'
+  apiURL: 'http://localhost:4040'
 };
 
 /*
